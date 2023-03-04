024d908bf0855eb137c3851a641d8e400162b438
hamza ezzat <hamza2007ezzat@gmail.com>
2023-03-04 23:21:51 +0100

    commit DB Connection

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import db from "../../../utils/db"
+
 export default function handler(req, res) {
+  console.log('ss')
+  db.connectDb();
+  db.disconnectDb();
   res.status(200).json({ name: 'John Doe' })
 }
