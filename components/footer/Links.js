a174bcff21c7dee8601d594b087e29dbecf1fe8c
hamza ezzat <hamza2007ezzat@gmail.com>
2023-03-10 22:48:06 +0100

    footer__socials

--- components/footer/Links.js
+++ components/footer/Links.js
@@ -1,97 +1,95 @@
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 import Link from "next/link";
 
-
 export default function Links() {
-    return (
-        <div className={styles.footer__links}>
-            {links.map((link,i)=> (                   
-                    <ul key={i}>
-                        {
-                            i === 0 
-                            ? <img src="../../../logo.png" alt=""/> 
-                            : <b>{link.heading}</b>
-                        }
-                        
-                        {link.links.map((link,i)=>(
-                            <li key={i}>
-                                <Link href={link.link}>{link.name}</Link>
-                            </li>
-                        ))
-                        }
-                    </ul>
-                ))
-            }
-        </div>
-    )
+  return (
+    <div className={styles.footer__links}>
+      {/* Change first header to logo (Logo in top of first section) */}
+      {links.map((link, i) => (
+        <ul key={i}>
+          {i === 1 ? (
+            <img src="../../../logo.png" alt="" />
+          ) : (
+            <b>{link.heading}</b>
+          )}
+
+          {link.links.map((link, i) => (
+            <li key={i}>
+              <Link href={link.link}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      ))}
+    </div>
+  );
 }
 
 const links = [
-    {
-      heading: "SHOPPAY",
-      links: [
-        {
-          name: "About us",
-          link: "",
-        },
-        {
-          name: "Contact us",
-          link: "",
-        },
-        {
-          name: "Social Responsibility",
-          link: "",
-        },
-        {
-          name: "",
-          link: "",
-        },
-      ],
-    },
-    {
-      heading: "HELP & SUPPORT",
-      links: [
-        {
-          name: "Shipping Info",
-          link: "",
-        },
-        {
-          name: "Returns",
-          link: "",
-        },
-        {
-          name: "How To Order",
-          link: "",
-        },
-        {
-          name: "How To Track",
-          link: "",
-        },
-        {
-          name: "Size Guide",
-          link: "",
-        },
-      ],
-    },
-    {
-      heading: "Customer service",
-      links: [
-        {
-          name: "Customer service",
-          link: "",
-        },
-        {
-          name: "Terms and Conditions",
-          link: "",
-        },
-        {
-          name: "Consumers (Transactions)",
-          link: "",
-        },
-        {
-          name: "Take our feedback survey",
-          link: "",
-        },
-      ],
-    },
-  ];
+  {
+    heading: "SHOPPAY",
+    links: [
+      {
+        name: "About us",
+        link: "",
+      },
+      {
+        name: "Contact us",
+        link: "",
+      },
+      {
+        name: "Social Responsibility",
+        link: "",
+      },
+      {
+        name: "",
+        link: "",
+      },
+    ],
+  },
+  {
+    heading: "HELP & SUPPORT",
+    links: [
+      {
+        name: "Shipping Info",
+        link: "",
+      },
+      {
+        name: "Returns",
+        link: "",
+      },
+      {
+        name: "How To Order",
+        link: "",
+      },
+      {
+        name: "How To Track",
+        link: "",
+      },
+      {
+        name: "Size Guide",
+        link: "",
+      },
+    ],
+  },
+  {
+    heading: "Customer service",
+    links: [
+      {
+        name: "Customer service",
+        link: "",
+      },
+      {
+        name: "Terms and Conditions",
+        link: "",
+      },
+      {
+        name: "Consumers (Transactions)",
+        link: "",
+      },
+      {
+        name: "Take our feedback survey",
+        link: "",
+      },
+    ],
+  },
+];
--- components/footer/index.js
+++ components/footer/index.js
@@ -1,4 +1,5 @@
 import Links from "./Links"
+import Socials from "./Socials"
 import styles from "./styles.module.scss"
 
 export default function Footer() {
@@ -6,6 +7,7 @@ export default function Footer() {
     <footer className={styles.footer}>
         <div className={styles.footer__container}>
             <Links />
+            <Socials />
         </div>
     </footer>
   )
