2300a353a2648fe468d7c9ecc6100aa8caa097bd
hamza ezzat <hamza2007ezzat@gmail.com>
2023-03-12 13:30:22 +0100

    Sign in file

--- components/inputes/loginInput/index.js
+++
@@ -1,7 +0,0 @@
-import { BiUser } from "react-icons/bi";
-import { SiMinteemailer } from "react-icons/si";
-import styles from "./styles.module.scss";
-
-export default function LoginInput({ icon }) {
-  return <div className={styles.input}>sss</div>;
-}
