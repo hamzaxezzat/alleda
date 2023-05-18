4f4b233fc003d2f5d50b3e17d1ed4ed6bee9b7d8
hamza ezzat <hamza2007ezzat@gmail.com>
2023-05-18 10:24:41 +0200

    clear API folders

--- pages/api/auth/hello.js
+++
@@ -1,9 +0,0 @@
-import nc from 'next-connect';
-
-const handler = nc();
-
-handler.post(async (req, res) => {
-  res.send('Welcom from Sign up api');
-});
-
-export default handler;
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,9 +1,14 @@
 import nc from 'next-connect';
+import db from '../../../utils/db';
 
 const handler = nc();
 
 handler.post(async (req, res) => {
   res.send('Welcom from Sign up api');
+  try {
+  } catch (error) {
+    res.statue(500).json({ message: error.message });
+  }
 });
 
 export default handler;
