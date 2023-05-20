3b23b4ddb2896c7f97121d502e5a446791106373
hamza ezzat <hamza2007ezzat@gmail.com>
2023-05-20 10:21:19 +0200

    edits

--- components/footer/Copyright.js
+++ components/footer/Copyright.js
@@ -1,49 +1,48 @@
-import Link from "next/link"
-import styles from "./styles.module.scss"
-import {IoLocationSharp} from "react-icons/io5";
+import Link from 'next/link';
+import styles from './styles.module.scss';
+import { IoLocationSharp } from 'react-icons/io5';
 
-export default function Copyright({country}) {
+export default function Copyright({ country }) {
   return (
     <div className={styles.footer__copyright}>
-        <section>© 2023 Alleda All Rights Reserved.</section>
-        <section>
-            <ul>
-                {data.map((link,i)=>(
-                        <li key={i}>
-                            <Link href={link.link}> {link.name}</Link>
-                        </li>
-                ))}
-                <li>
-                    <a>
-                        <IoLocationSharp /> {country.name}
-                    </a>
-                </li>
-            </ul>
-            </section>
+      <section>© 2023 Alleda All Rights Reserved.</section>
+      <section>
+        <ul>
+          {data.map((link, i) => (
+            <li key={i}>
+              <Link href={link.link}>{link.name}</Link>
+            </li>
+          ))}
+          <li>
+            <a>
+              <IoLocationSharp /> {country.name}
+            </a>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
 
-
 const data = [
-    {
-      name: "Privacy Center",
-      link: "",
-    },
-    {
-      name: "Privacy & Cookie Policy",
-      link: "",
-    },
-    {
-      name: "Manage Cookies",
-      link: "",
-    },
-    {
-      name: "Terms & Conditions",
-      link: "",
-    },
-    {
-      name: "Copyright Notice",
-      link: "",
-    },
-  ];
+  {
+    name: 'Privacy Center',
+    link: '',
+  },
+  {
+    name: 'Privacy & Cookie Policy',
+    link: '',
+  },
+  {
+    name: 'Manage Cookies',
+    link: '',
+  },
+  {
+    name: 'Terms & Conditions',
+    link: '',
+  },
+  {
+    name: 'Copyright Notice',
+    link: '',
+  },
+];
--- components/header/Ad.js
+++ components/header/Ad.js
@@ -1,10 +1,10 @@
-import Link from "next/link"
-import styles from "./styles.module.scss"
+import Link from 'next/link';
+import styles from './styles.module.scss';
 
 export default function Ad() {
   return (
     <Link href="./browse" legacyBehavior>
-        <div className={styles.ad}></div>
+      <div className={styles.ad}></div>
     </Link>
   );
 }
