c9c739feb09a7cf2c56070a69415e1264b8c112a
hamza ezzat <hamza2007ezzat@gmail.com>
2023-05-22 14:57:42 +0200

    signin authorization

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,7 +1,9 @@
 import nc from 'next-connect';
+import bcrypt from 'bcrypt';
 import db from '../../../utils/db';
 import User from '../../../models/User';
 import { validateEmail } from '../../../utils/validation';
+import { createActivationToken } from '../../../utils/tokens';
 const handler = nc();
 
 handler.post(async (req, res) => {
@@ -19,6 +21,21 @@ handler.post(async (req, res) => {
     if (user) {
       return res.status(400).json({ message: 'This Email already exsits' });
     }
+    if (password.length < 6) {
+      return res.status(400).json({ message: 'This Password is short' });
+    }
+    const bcryptPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: bcryptPassword });
+    // .save() : Add to DB
+    const addedUser = await newUser.save();
+
+    // To generate Token to verify email
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
+    console.log(activation_token);
+    res.send(activation_token);
+    // res.send(addedUser);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
