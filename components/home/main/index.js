5268aac8f4554f670994ab442751291e53545c72
hamza ezzat <hamza2007ezzat@gmail.com>
2023-05-26 15:56:59 +0200

    commit

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,3 +1,4 @@
+import Offers from './offers';
 import styles from './styles.module.scss';
 import MainSwiper from './swiper';
 
@@ -7,7 +8,7 @@ export default function Main() {
       <div className={styles.header}>header</div>
       <div className={styles.menu}>menu</div>
       <MainSwiper />
-      <div className={styles.offers}>offers</div>
+      <Offers />
       <div className={styles.user}>user</div>
     </div>
   );
---
+++ components/home/main/offers.js
@@ -0,0 +1,38 @@
+import styles from './styles.module.scss';
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation } from 'swiper';
+
+export default function Offers() {
+  return (
+    <div className={styles.offers}>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Navigation]}
+        className="offers_swiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
