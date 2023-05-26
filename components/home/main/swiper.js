f1dbcda2d662e0b9f2aa3c7c433170f2063830a8
hamza ezzat <hamza2007ezzat@gmail.com>
2023-05-26 10:53:55 +0200

    commit works

--- components/home/main/swiper.js
+++ components/home/main/swiper.js
@@ -1,5 +1,40 @@
 import styles from './styles.module.scss';
 
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
+
 export default function MainSwiper() {
-  return <div className={styles.swiper}>swiper</div>;
+  return (
+    <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="swiper mainSwiper"
+      >
+        {Array.from(Array(10), (e, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={`../../../images/swiper/${i + 1}.jpg`} />
+            </SwiperSlide>
+          );
+        })}
+
+        {/* <swiperHolder /> */}
+        {/* <SwiperSlide>Slide 2</SwiperSlide> */}
+      </Swiper>
+    </>
+  );
 }
