0a6e6997ca69ae0c65bb70c9b88953197032731f
hamza ezzat <hamza2007ezzat@gmail.com>
2023-05-31 10:55:22 +0200

    main / header

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,13 +1,15 @@
+import Link from 'next/link';
 import User from './User';
 import Menu from './menu';
 import Offers from './offers';
 import styles from './styles.module.scss';
 import MainSwiper from './swiper';
+import Header from './Header';
 
 export default function Main() {
   return (
     <div className={styles.main}>
-      <div className={styles.header}>header</div>
+      <Header />
       <Menu />
       <MainSwiper />
       <Offers />
