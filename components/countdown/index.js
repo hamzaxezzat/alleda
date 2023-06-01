40b2be12f1d9f579cfe798c7d52e9cfee0531a8a
hamza ezzat <hamza2007ezzat@gmail.com>
2023-06-01 20:15:01 +0200

    Commit

--- components/countdown/index.js
+++ components/countdown/index.js
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
+import { calculeteDiff } from './utils';
 
-function Countdown() {
+const defaultRemainingTime = {
+  seconds: '00',
+  minutes: '00',
+  hours: '00',
+  days: '00',
+};
+export default function Countdown({ date }) {
+  const [timeInMs, setTimeInMs] = useState(date.getTime());
+  const [remainingTime, setRemainingTime] = useState();
+  useEffect(() => {
+    setTimeInMs(date.getTime), [date];
+  });
+  const dateImMs = date.getTime();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateRemainingTime(timeInMs);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeInMs]);
+  const updateRemainingTime = (timeInMs) => {
+    setRemainingTime(calculeteDiff(timeInMs));
+  };
   return (
     <div className={styles.countdown}>
       <span>1</span>
@@ -11,8 +35,7 @@ function Countdown() {
       <b>:</b>
       <span>1</span>
       <span>0</span>
+      <span>{dateImMs}</span>
     </div>
   );
 }
-
-export default Countdown;
