6a3047a25fe3b39013a8bbe9422251fce30d449d
hamza ezzat <hamza2007ezzat@gmail.com>
2023-06-05 10:37:08 +0200

    Product Schema !Important

--- models/Product.js
+++ models/Product.js
@@ -1,65 +1,129 @@
 import mongoose from 'mongoose';
-
 const { ObjectId } = mongoose.Schema;
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  brand: {
-    type: String,
-  },
-  slug: {
-    type: String,
-    required: true,
-  },
-  category: {
+const reviewSchema = new mongoose.Schema({
+  reviewBy: {
     type: ObjectId,
+    ref: 'User',
     required: true,
-    ref: 'Category',
   },
-  subCategories: [
-    {
-      type: [ObjectId],
-      ref: 'SubCategory',
-    },
-  ],
-  details: [{}],
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  review: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+  },
+  style: {
+    color: String,
+    image: String,
+  },
+  fit: {
+    type: String,
+  },
+  images: [],
+  likes: [],
 });
-
-// const Product = new mongoose.Schema({
-//   name: {
-//     type: String,
-//   },
-//   price: {
-//     type: Number,
-//   },
-//   equipped: Boolean,
-//   owner_id: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     index: true,
-//   },
-//   room_id: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     index: true,
-//   },
-//   brand:{
-//       type:String
-//   },
-//   color:{}
-//   rating:[
-//       {
-//             reviwer
-//       }
-//   ]
-// });
-
-// const Item = mongoose.model('Item', Product);
-
-// module.exports = {
-//   Item: Item,
-// };
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: ObjectId,
+      required: true,
+      ref: 'Category',
+    },
+    subCategories: [
+      {
+        type: [ObjectId],
+        ref: 'SubCategory',
+      },
+    ],
+    // Dynamic Details to add individual details
+    details: [
+      {
+        name: String,
+        value: String,
+      },
+    ],
+    questions: [
+      {
+        question: String,
+        answer: String,
+      },
+    ],
+    reviews: [reviewSchema],
+    refundPolicy: {
+      type: String,
+      default: '30 days',
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    shipping: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    // Same Product put e.x one color has discount
+    subProducts: [
+      {
+        images: [],
+        description_images: [],
+        color: {
+          color: {
+            type: String,
+          },
+          image: {
+            type: String,
+          },
+        },
+        sizes: [
+          {
+            size: String,
+            qty: Number,
+            price: Number,
+          },
+        ],
+        discount: {
+          type: Number,
+          default: 0,
+        },
+        sold: {
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+const Product =
+  mongoose.models.Product || moongoose.model('Product', productSchema);
+export default Product;
